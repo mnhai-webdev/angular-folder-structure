eb3e2725302d29931d41f715c7c358b53ae49c8e
haimn <mnhai@vnpt.vn>
2021-07-11 23:12:19 +0700

    Add loading component in shared

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { AdminArticleListComponent } from './admin-article-list/admin-article-list.component';
+import { LoadingModule } from '../../shared/components/loading/loading.module';
 
 
 @NgModule({
   declarations: [AdminComponent, AdminArticleListComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    LoadingModule
   ]
 })
 export class AdminModule { }
--- src/app/modules/article/article.module.ts
+++ src/app/modules/article/article.module.ts
@@ -5,13 +5,15 @@ import { ArticleRoutingModule } from './article-routing.module';
 import { ArticleComponent } from './article.component';
 import { ArticleDetailComponent } from './article-detail/article-detail.component';
 import { ArticleListComponent } from './article-list/article-list.component';
+import { LoadingModule } from '../../shared/components/loading/loading.module';
 
 
 @NgModule({
   declarations: [ArticleComponent, ArticleDetailComponent, ArticleListComponent],
-  imports: [
-    CommonModule,
-    ArticleRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        ArticleRoutingModule,
+        LoadingModule
+    ]
 })
 export class ArticleModule { }
